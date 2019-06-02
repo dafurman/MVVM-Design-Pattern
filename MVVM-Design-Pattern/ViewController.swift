f6704a9c00a5edb4c9692921781653aaf34865b5
David Furman <DavidAsherFurman@gmail.com>
2019-06-02 14:32:06 -0700

    Initial Commit

---
+++ MVVM-Design-Pattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-Design-Pattern
+//
+//  Created by David Furman on 6/2/19.
+//  Copyright © 2019 David Furman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
